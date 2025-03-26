809a31703d0d94f1bd51311d23e741b9b2983508
Rc0fficial <to.arsalanali@gmail.com>
2025-03-26 12:51:40 +0500

    Fix: Wizard forms validations & functional

--- src/components/common/SelectField/index.jsx
+++ src/components/common/SelectField/index.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const SelectField = ({ label, value, onChange, options }) => {
-    return (
-        <div className='flex flex-col gap-3'>
-            <label htmlFor={label} className='text-525 leading-none text-center'>{label}</label>
-            <select
-                value={value}
-                onChange={onChange}
-                className='h-12 bg-[rgb(250,250,250)] px-4 rounded-md text-[#989898] border-[#BDBDBD] placeholder:text-[#989898] border-[0.5px]'
-                id={label}
-            >
-                <option value="">Select {label}</option>
-                {options.map((option, index) => (
-                    <option key={index} value={option.value}>
-                        {option.label}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
+const SelectField = ({ label, value, onChange, options, error, onBlur }) => {
+  return (
+    <div className="flex flex-col gap-3">
+      <label htmlFor={label} className="text-525 leading-none text-start">
+        {label}
+      </label>
+      <select
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        className={`h-12 bg-[rgb(250,250,250)] px-4 rounded-md text-[#989898] border-[#BDBDBD] placeholder:text-[#989898] border-[0.5px] ${
+          error ? "border-red-500" : ""
+        }`}
+        id={label}
+      >
+        <option value="">Select {label}</option>
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </div>
+  );
 };
 
 export default SelectField;
